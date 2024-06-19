a8b64495bc6472842846f2ebd8adadcb5e72920e
yonokuso <qltenwkd@naver.com>
2024-06-20 04:07:14 +0900

    9-2 RaycastHit
    240620 VR Setting, RaycastHit

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class PlayerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position,transform.forward, out hit))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
